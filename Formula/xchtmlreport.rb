53dbaaae6b6f40a937944e1753e6952fef8f3148
Julien Rollet <julien.rollet@fabernovel.com>
2019-10-17 13:41:00 +0200

    Bump xchtmlreport formula

--- Formula/xchtmlreport.rb
+++ Formula/xchtmlreport.rb
@@ -1,12 +1,12 @@
 class Xchtmlreport < Formula
   desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
   homepage "https://github.com/applidium/XCTestHTMLReport"
-  url "https://github.com/applidium/XCTestHTMLReport/archive/1.7.3.tar.gz"
-  sha256 "1cff2878812c22a2b54071b941c406c2560c9eb7b10356bcafd01131bdaad23f"
+  url "https://github.com/applidium/XCTestHTMLReport/archive/2.0.0.tar.gz"
+  sha256 "99ce892200ac116ee551ab609045983070165b706cac08391f294c979ceab448"
   head "https://github.com/applidium/XCTestHTMLReport.git", :branch => "develop_ad"
 
   def install
-    system "xcodebuild" " clean" " build" " CODE_SIGNING_REQUIRED=NO" " -workspace" " XCTestHTMLReport.xcworkspace" " -scheme XCTestHTMLReport" " -configuration Release"
-    bin.install "xchtmlreport"
+    system "swift build --disable-sandbox -c release"
+    bin.install ".build/release/xchtmlreport"
   end
 end
