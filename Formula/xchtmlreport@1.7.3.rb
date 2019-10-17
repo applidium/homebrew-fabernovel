6923be296f2cca4f760092deb00c26863a555ba7
Julien Rollet <julien.rollet@fabernovel.com>
2019-10-17 11:27:18 +0200

    Create a fixed version formula of xchtmlreport

---
+++ Formula/xchtmlreport@1.7.3.rb
@@ -0,0 +1,12 @@
+class XchtmlreportAT173 < Formula
+  desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
+  homepage "https://github.com/applidium/XCTestHTMLReport"
+  url "https://github.com/applidium/XCTestHTMLReport/archive/1.7.3.tar.gz"
+  sha256 "1cff2878812c22a2b54071b941c406c2560c9eb7b10356bcafd01131bdaad23f"
+  head "https://github.com/applidium/XCTestHTMLReport.git", :branch => "develop_ad"
+
+  def install
+    system "xcodebuild" " clean" " build" " CODE_SIGNING_REQUIRED=NO" " -workspace" " XCTestHTMLReport.xcworkspace" " -scheme XCTestHTMLReport" " -configuration Release"
+    bin.install "xchtmlreport"
+  end
+end
